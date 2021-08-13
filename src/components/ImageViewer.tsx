05fcca5a401881094bf3e0b280ee85fc631ee3ff
Mark Sanford <marktsanford@gmail.com>
2021-08-13 16:10:21 -0700

    selector in imageviewer

--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -1,20 +1,29 @@
 import { ipcRenderer } from 'electron';
-import { isAbsolute } from 'path';
 import React, { useContext, useEffect, useReducer } from 'react';
-// import { Fragment } from 'react';
 import { useImmerReducer } from 'use-immer';
 import { current } from 'immer';
 import { pathToUrl } from '../lib/util';
 
 import CurrentImageContext from '../store/current-image-context';
+import { Action } from 'history';
 
 const imagePositionDefault = {
   dragging: false,
+  startMousePos: {
+    x: 0,
+    y: 0,
+  },
   lastMousePos: {
     x: 0,
     y: 0,
   },
   clientRect: {
+    left: 0,
+    right: 0,
+    width: 0,
+    height: 0,
+  },
+  selectRect: {
     left: 0,
     right: 0,
     width: 0,
@@ -28,29 +37,58 @@ const imagePositionDefault = {
 
 // IMPORTANT: Using immer!
 const imagePositionReducer = (state, action) => {
+  let mousePos = { x: 0, y: 0 };
+  if (action.payload.clientRect && action.payload.mousePos) {
+    mousePos = {
+      x: action.payload.mousePos.x - action.payload.clientRect.left,
+      y: action.payload.mousePos.y - action.payload.clientRect.top,
+    };
+  }
+
   if (action.type === 'mouseDown') {
-    state.lastMousePos = action.payload.pos;
+    state.startMousePos = mousePos;
     state.dragging = true;
+
+    state.selectRect = {
+      left: mousePos.x,
+      top: mousePos.y,
+      width: 0,
+      height: 0,
+    };
+
     return;
   }
+
+  if (action.type === 'mouseMove' || action.type === 'mouseUp') {
+    if (state.dragging) {
+      state.selectRect = {
+        left: Math.min(state.startMousePos.x, mousePos.x),
+        top: Math.min(state.startMousePos.y, mousePos.y),
+        width: Math.abs(mousePos.x - state.startMousePos.x),
+        height: Math.abs(mousePos.y - state.startMousePos.y),
+      };
+
+      // state.imageOffset = {
+      //   x: state.imageOffset.x + action.payload.pos.x - state.lastMousePos.x,
+      //   y: state.imageOffset.y + action.payload.pos.y - state.lastMousePos.y,
+      // };
+      // state.lastMousePos = mousePos;
+    }
+  }
+
   if (action.type === 'mouseMove') {
-    if (state.dragging) {
-      state.imageOffset = {
-        x: state.imageOffset.x + action.payload.pos.x - state.lastMousePos.x,
-        y: state.imageOffset.y + action.payload.pos.y - state.lastMousePos.y,
-      };
-      state.lastMousePos = action.payload.pos;
-    }
     return;
   }
+
   if (action.type === 'mouseUp') {
-    if (state.dragging) {
-      state.imageOffset = {
-        x: state.imageOffset.x + action.payload.pos.x - state.lastMousePos.x,
-        y: state.imageOffset.y + action.payload.pos.y - state.lastMousePos.y,
-      };
-      state.dragging = false;
-    }
+    // if (state.dragging) {
+    //   state.imageOffset = {
+    //     x: state.imageOffset.x + action.payload.pos.x - state.lastMousePos.x,
+    //     y: state.imageOffset.y + action.payload.pos.y - state.lastMousePos.y,
+    //   };
+    //   state.dragging = false;
+    // }
+    state.dragging = false;
     // console.log(current(state));
     return;
   }
@@ -72,59 +110,69 @@ const ImageViewer = (props) => {
     console.log(imageInfo);
   }, [imagePath]);
 
-  const imageStyles = {
-    position: 'absolute',
-    width: '200px',
-    left: `${positionState.imageOffset.x}px`,
-    top: `${positionState.imageOffset.y}px`,
-    pointerEvents: 'none',
-  };
-
   const mouseDownHandler = (event) => {
     const clientRect = event.target.getBoundingClientRect();
-    const pos = { x: event.clientX, y: event.clientY };
+    const mousePos = { x: event.clientX, y: event.clientY };
     positionDispatch({
       type: 'mouseDown',
       payload: {
         clientRect,
-        pos,
+        mousePos,
       },
     });
   };
 
   const mouseMoveHandler = (event) => {
     const clientRect = event.target.getBoundingClientRect();
-    const pos = { x: event.clientX, y: event.clientY };
+    const mousePos = { x: event.clientX, y: event.clientY };
     positionDispatch({
       type: 'mouseMove',
       payload: {
         clientRect,
-        pos,
+        mousePos,
       },
     });
   };
 
   const mouseUpHander = (event) => {
     const clientRect = event.target.getBoundingClientRect();
-    const pos = { x: event.clientX, y: event.clientY };
+    const mousePos = { x: event.clientX, y: event.clientY };
     positionDispatch({
       type: 'mouseUp',
       payload: {
         clientRect,
-        pos,
+        mousePos,
       },
     });
   };
 
+  const imageStyles = {
+    position: 'absolute',
+    width: '500px',
+    left: `${positionState.imageOffset.x}px`,
+    top: `${positionState.imageOffset.y}px`,
+    pointerEvents: 'none',
+  };
+
+  const selectStyles = {
+    position: 'absolute',
+    left: `${positionState.selectRect.left}px`,
+    top: `${positionState.selectRect.top}px`,
+    width: `${positionState.selectRect.width}px`,
+    height: `${positionState.selectRect.height}px`,
+    visibility: positionState.dragging ? 'visible' : 'hidden',
+    pointerEvents: 'none',
+  };
+
   return (
     <div className="image-viewer">
+      <div className="image-viewer-select" style={selectStyles} />
       {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
       <div
         className="image-viewer-overlay"
         onMouseDown={mouseDownHandler}
         onMouseMove={mouseMoveHandler}
         onMouseUp={mouseUpHander}
-        // style={{ backgroundImage: `url(${imageUrl})` }}
       />
       <img
         className="image-viewer-image"
